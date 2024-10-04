1ba77016a5f1cbd4b3c788a6ec9a20132a0f28e4
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-10-04 16:05:18 +0530

    Add files via upload

---
+++ levin.py
@@ -0,0 +1,37 @@
+
+# program1
+
+#print("Hello \nworld")
+"""
+if 5>2: #
+    print("five is grater than two")
+
+if 5>2: #
+    print("five is grater than two")
+
+x = 1
+x = "thomas"
+print(x)
+
+x =str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+print(type(x))
+
+x, y, z ="orange", "banana", "cherry"
+print(x,y,z)
+
+x = "python "
+y = "is "
+z = "awsome"
+print(x+y+z)
+
+
+username = input("Enter your username \n")
+print ("your name is " +username)
+"""
+a = "hello world"
+print(a[:5])
