3a1ff61b3de916d9285000e79fd55b7e4e66f02c
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-10-18 22:02:03 +0530

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,12 @@
+"""
+Author : Levin Abraham Jacob
+Date : 18 - 10- 2024
+program that uses functions from maths module to perform the following operations given by the user:
+"""
+import math
+num = int(input ("Enter A Number \n"))
+sqrt = math.sqrt(num)
+
+print("square root of the number ", num ,"=" , sqrt)
+print("factorial of ", num ,"=" , math.factorial(num))
+print(num , "Number Raised to power 2 =" , math.pow(num,2))
