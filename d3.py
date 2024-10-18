767952af0b757e362527d14ef917023997d65b82
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-10-18 22:16:24 +0530

    Add files via upload

---
+++ d3.py
@@ -0,0 +1,20 @@
+"""
+Author = levin abraham jacob
+date = 18-10-2024
+"""
+
+number1 = int(input("Enter the first number:"))
+
+number2 = int(input("Enter the second number:"))
+
+
+
+print("SUM=", number1+number2, "Divison=", number1/number2)
+
+print("Is number1 greater than number2?:", number1>number2)
+
+print("number1 and number 2 equal?", number1==number2)
+
+print("Logical AND ", number1>0 and number2>0)
+
+print("Logical OR ", number1>0 or number2>10)
