3c925c9cef06a4770b9f621d5e9bcf6f454fb1d6
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-10-18 22:20:16 +0530

    Add files via upload

---
+++ d4.py
@@ -0,0 +1,19 @@
+"""
+Author = Levin Abraham Jacob
+date = 18-10-2024
+Description: program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+
+"""
+
+str1 = "Hello, World"
+
+str2 = str1[7:12]
+
+print(str2)
+
+
+
+str3 = "everyone!"
+
+
+print(str2+" "+str3)
