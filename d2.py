d2c91326239f89a41fafc823b7ee76016e8cb27c
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-10-18 22:10:13 +0530

    Add files via upload

---
+++ d2.py
@@ -0,0 +1,9 @@
+"""
+Author = Levin Abraham Jacob
+Date = 18-10-2024
+Description = program to ask user to input their name and age . After receiving the input , display
+a message that include users name and age
+"""
+name= input("Enter your name \n")
+age= input("Enter your age \n")
+print("Hello ",name,"you are ",age,"years old")
