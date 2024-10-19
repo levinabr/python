3ea4ae9da5d04420992e7f1b134ae1b1e4b5aa3c
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-10-19 14:53:21 +0530

    Update oct19le2.py

--- oct19le2.py
+++ oct19le2.py
@@ -3,8 +3,8 @@ name = Levin Abraham Jacob
 Title: Create, concatenate,
 and print a string and access a sub-string from a given string.
 """
-x = input("First Name \n ")
-y = input("Second name \n ")
+x = input("Enter your First Name \n ")
+y = input("Last name \n ")
 z = x+" "+y
 print(z)
 print("the last name is ",y[0:])
