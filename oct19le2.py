88f6696fe57b25ffe919ce803f07c09f03fa9d1e
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-10-19 14:39:36 +0530

    Add files via upload

---
+++ oct19le2.py
@@ -0,0 +1,9 @@
+"""
+name = Levin Abraham Jacob
+Title: Create, concatenate,
+and print a string and access a sub-string from a given string.
+"""
+x = "Levin Abraham "
+y = "Jacob"
+z = x+y
+print(z)
