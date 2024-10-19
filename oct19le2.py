330b8890834cad357aebab25a53549865db9a43f
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-10-19 15:21:25 +0530

    Add files via upload

---
+++ oct19le2.py
@@ -0,0 +1,10 @@
+"""
+name = Levin Abraham Jacob
+Title: Create, concatenate,
+and print a string and access a sub-string from a given string.
+"""
+x = input("First Name \n ")
+y = input("Second name \n ")
+z = x+" "+y
+print(z)
+print("the sub string is", z[:len(x)])
