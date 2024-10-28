f076b3fb6ea407cae31fda2744dd4c60bd06173e
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-10-28 10:38:26 +0530

    oct28lab2.py

--- oct28lab2.py
+++ oct28lab2.py
@@ -1,17 +1,39 @@
-temp = int(input("enter the temperature\n"))
-print("Enter unit of your temperature")
-print("1.\t Celsius  ")
-print("2.\tFahrenheit")
-print("3.\texit()")
-choice = int(input("Enter Your Unit Of Temperature \n"))
+"""
+Author = Levin Abraham Jacob
+date = 28 / 10/2024
+Description = Write a menu-driven Python program that allows users to convert temperatures
+between Celsius and Fahrenheit.
+The program should repeatedly display a menu with three options:
+
+    Convert Celsius to Fahrenheit
+    Convert Fahrenheit to Celsius
+    Exit the program
+
+Based on the user's choice:
+
+    If they select option 1, prompt them to enter a temperature in Celsius and
+    display the equivalent temperature in Fahrenheit.
+    If they select option 2, prompt them to enter a temperature in Fahrenheit and
+    display the equivalent temperature in Celsius.
+    If they select option 3, exit the program.
+
+
+"""
 while True :
-    if choice ==1:
+    temp = int(input("Enter The Temperature\n"))
+    print("c.\t Celsius  ")
+    print("f.\tFahrenheit")
+    print("e.\texit()")
+    choice = (input("Enter Your Unit Of Temperature \n"))
+    if choice =="c":
         f = (temp * 9 / 5) + 32
-        print(f, "is the converted temperature")
+        print(f, "is the converted temperature\n")
+
+    elif choice =="f":
+        c = 5/9* (temp - 32)
+        print(c, "is the converted temperature\n")
+
+    elif choice =="e" :
         break
-    elif choice ==2 :
-        c = 9 / 5 * (temp - 32)
-        print(c, "is the converted temperature")
-        break
-    elif choice ==3 :
-        break
+    else:
+        print("Invalid Temperature")
